7dd9a2fc2a665c41130c94a6400a32211dcb0fee
get --version <get -version>
2013-02-13 21:30:32 -0500

    encontre aplicacion

--- app/models/fmalbum.rb
+++ app/models/fmalbum.rb
@@ -1,11 +1,11 @@
 class Fmalbum < ActiveRecord::Base
 	require 'lastfm'
 
-	lastfm = Lastfm.new('no encuentro el codigo', 'no encuentro el codigo')
+	lastfm = Lastfm.new('1207594aba255b1543e706b41a99abd7', '1501885a000b9fcf1ef4d03128b90bd9')
 	token = lastfm.auth.get_token
 
 	# open 'http://www.last.fm/api/auth/?api_key=xxxxxxxxxxx&token=xxxxxxxx' and grant the application
 
-	lastfm.session = lastfm.auth.get_session(:token => token)['key']
+	lastfm.session = lastfm.auth.get_session(:token => token)['1207594aba255b1543e706b41a99abd7']
 
 end
