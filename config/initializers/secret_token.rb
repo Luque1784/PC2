514f0dd583b377839b4c9432f51c232b3819298d
get --version <get -version>
2013-02-13 19:58:20 -0500

    PC02

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LastFM::Application.config.secret_token = '8b4fd2a5bd04ab4d3a255741d911fef92a9e93fa31e638a09de5d5bea8c2d1af277bbff555ef0467f95fa2079390fc5095879df5b15cea165a0c6dd06524c29f'
